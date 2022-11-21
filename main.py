1ebe9607c34a36be6cb540c06d7c1b7a35677cf8
スケッチ <54027485+suketti@users.noreply.github.com>
2022-11-21 10:19:35 +0100

    Implement basic menu system

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("hello world")
+import msvcrt
+import sys
+import os
+
+
+def main():
+    name = input("Adja meg a nevét ")
+    os.system("cls")
+    while True:
+        print("Kérjük válasszon az alábbi menűpontok közül")
+        print("1. Commit létrehozása")
+        print("2. Az aktív commit megváltoztatása")
+        print("3. Verziók listázása")
+        print("ESC: Kilépés")
+
+        inp = msvcrt.getch()
+
+        if inp == chr(27).encode():
+            sys.exit()
+        if inp == "1".encode():
+            print("menu 1")
+        if inp == "2".encode():
+            print("menu 2")
+        if inp == "3".encode():
+            print("menu 3")
+
+        os.system("cls")
+
+if __name__ == "__main__":
+    main()
+
+
+    
