6b6463a14da5679ec32a6735747a492b66a99d60
Géczi Kornél <hungarokornel@gmail.com>
2022-11-23 14:59:30 +0100

    Implement current commit changing.

--- functions/change_current_commit.py
+++ functions/change_current_commit.py
@@ -5,7 +5,8 @@ import shutil
 def current_commit_version(folder) -> int:
     path = f"{folder}/.dusza/head.txt"
     if not os.path.exists(path):
-        return print("[Error] The chosen folder is not initialized.")
+        print("[Error] The chosen folder is not initialized.")
+        return
     with open(path, 'r', encoding="utf-8") as f:
         return int(f.readlines()[0].strip())
 
@@ -14,18 +15,24 @@ def change_current_commit_version(folder, new_version):
     current_version = current_commit_version(folder)
     path = f"{folder}/.dusza/{new_version}.commit"
     if current_version == new_version:
-        return "The chosen version mustn't be equal with the new version."
+        print("The chosen version mustn't be equal with the new version.")
+        return
     if not os.path.exists(path):
-        return "The chosen version is not valid."
+        print("The chosen version is not valid.")
+        return
 
     for i in os.listdir(folder):
         if i == ".dusza":
             continue
-        # os.remove(f"{folder}/{i}")
-    for file_name in os.listdir(f"{folder}/.dusza/{new_version}.commit"):
-        source = f"{folder}/.dusza/{new_version}.commit"
-        destination = folder
-        shutil.copytree(source, destination, ignore=shutil.ignore_patterns("commit.details"), dirs_exist_ok=True)
-        print('copied', file_name)
+        pp = f"{folder}/{i}"
+        if os.path.isdir(pp):
+            shutil.rmtree(pp)
+        else:
+            os.remove(pp)
+
+    shutil.copytree(path, folder, ignore=shutil.ignore_patterns("commit.details"), dirs_exist_ok=True)
+
+    with open(folder+ ".dusza/head.txt", "w") as f:
+        f.write(str(new_version))
 
     return
